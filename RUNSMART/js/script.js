f95b66d8157dea126f91277e36819e499becdf5b
Jakhongir <jakhongir.ikromidinov@gmail.com>
2022-07-27 16:55:53 +0500

    the problem with background images in the 'runsmart' section  was solved. also added new catalog section with its js script into 'runsmart'

--- RUNSMART/js/script.js
+++ RUNSMART/js/script.js
@@ -5,3 +5,21 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right.png"></button>' 
     });
 });
+let moreLink = document.querySelectorAll('.catalog-item__link');
+let backLink = document.querySelectorAll('.catalog-item__back');
+
+for (let link of moreLink) {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        this.parentElement.classList.remove('catalog-item__content_active');
+        this.parentElement.nextElementSibling.classList.add('catalog-item__list_active');
+        for (let link of backLink) {
+            link.addEventListener('click', function(event) {
+                event.preventDefault();
+                console.log('done');
+                this.parentElement.classList.remove('catalog-item__list_active');
+                this.parentElement.previousElementSibling.classList.add('catalog-item__content_active');
+            })
+        }
+    })
+}
